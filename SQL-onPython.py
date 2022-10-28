4b51d731bfbc6cf9098b40e154749bd036c09c37
Andres Leon <redwoodtrees4@outlook.com>
2022-10-28 13:01:03 -0400

    Created DB called Smartphones
    Used sqlite3 to create local server-less DB and created new columns and records

--- SQL-onPython.py
+++ SQL-onPython.py
@@ -7,18 +7,54 @@
 # py -m venv venv to create a new virtual environment
 
 # required modules
-# please install anaconda in order to install Pandas: https://www.anaconda.com/
-# please install using cmd Prompt:
-# pip install openpyxl
-# pip install pandas
-from csv import excel
-import pandas as pd
-excel_file = 'HD2020.xlsx'
+# sqlite3
+# to view DB created please install extension SQLite Viewer from extensions Tab
+# import sqlite3
+# con = sqlite3.connect("tutorial.db")
+# # cursor to execute SQL statements, like an actual mouse cursor for placement
+# cur = con.cursor()
 
-df = pd.read_excel(excel_file)
-print(df.head(10)) # prints first 10 items from data set
-print('--------------------------------------',
-'\n', 'All columns in data set')
-print(df.info())
+# #cur.execute("CREATE TABLE movie(Title, Year, Score)") # When creating a new table, datatypes are optional thanks to something called Flexible typing
+# # YOUR ACTUAL SQL STATEMENT GOES HERE
+# # SQLite3 creates a master DB to reference from
+# res = cur.execute("SELECT name FROM sqlite_master WHERE name='spam'")
+# res.fetchone() is None
 
-#df.loc[df['STABBR'] == 'NC'].sum()
+
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(path):
+    connection = None
+    try:
+        connection = sqlite3.connect(path)
+        print("Connection to SQLite DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+    return connection
+
+connection = create_connection("SmartphoneInventory.db")
+ 
+cur = connection.cursor()
+# Only uncomment out this line to create a NEW TABLE
+# cur.execute("CREATE TABLE SmartPhones(Brand, Model, InternalMemory, ScreenSize, RAM, Color, Price, SKU, ReleaseDate)")
+#print("Table Successfully Created!")
+
+# Insert records into the newly created columns here
+# cur.execute("""
+#     INSERT INTO SmartPhones VALUES
+#         ('Apple', 'iPhone 14', '128GB',  6.1, '8GB', 'Space Grey', 1099.99, 000001, '09/22/2022')
+# """)
+#print("Records have been added, refresh the DB to see changes.")
+
+
+
+# insert many records into the DB here
+
+
+# Query search here
+res = cur.execute("SELECT Model FROM SmartPhones")
+showResult = res.fetchall()
+print(showResult)
+# saves the changes to the database
+#connection.commit()
