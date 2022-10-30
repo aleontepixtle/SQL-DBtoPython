c2839d245b2b3794951606438205fc440e319d18
Andres Leon <redwoodtrees4@outlook.com>
2022-10-29 22:11:03 -0400

    Edited

--- SQL-onPython.py
+++ SQL-onPython.py
@@ -21,6 +21,7 @@
 # res.fetchone() is None
 
 
+from itertools import count
 import sqlite3
 from sqlite3 import Error
 
@@ -28,7 +29,9 @@ def create_connection(path):
     connection = None
     try:
         connection = sqlite3.connect(path)
-        print("Connection to SQLite DB successful")
+        print("\n\n####################################", '\n',
+              "Connection to SQLite DB successful", '\n',
+              "####################################\n\n", sep="")
     except Error as e:
         print(f"The error '{e}' occurred")
     return connection
@@ -37,24 +40,54 @@ connection = create_connection("SmartphoneInventory.db")
  
 cur = connection.cursor()
 # Only uncomment out this line to create a NEW TABLE
-# cur.execute("CREATE TABLE SmartPhones(Brand, Model, InternalMemory, ScreenSize, RAM, Color, Price, SKU, ReleaseDate)")
+#ur.execute("CREATE TABLE SmartPhone(Brand, Model, InternalMemory, ScreenSize, RAM, Color, Price, Carrier, SKU, UPC)")
 #print("Table Successfully Created!")
 
-# Insert records into the newly created columns here
+# Insert ONE set of data into the newly created columns here
 # cur.execute("""
 #     INSERT INTO SmartPhones VALUES
-#         ('Apple', 'iPhone 14', '128GB',  6.1, '8GB', 'Space Grey', 1099.99, 000001, '09/22/2022')
+#         ('Apple', 'iPhone 14', '128GB',  6.1, '8GB', 'Space Grey', 1099.99, 000001, 'AT&T')
 # """)
 #print("Records have been added, refresh the DB to see changes.")
 
 
 
-# insert many records into the DB here
+# insert MANY sets of data into the DB here
+##############################################
+# rows = [('Apple', 'iPhone 14', '128GB', 6.1, '8GB', 'Space Grey', 1099.99, 'AT&T', 'A10001A', 1000),
+#     ('Apple', 'iPhone 14', '128GB', 6.1, '8GB', 'Deep Purple', 1099.99, 'AT&T', 'A10001A', 1001),
+#     ('Apple', 'iPhone 14', '128GB', 6.1, '8GB', 'Alpine Green', 1099.99, 'AT&T', 'A10001A', 1002),
+#     ('Apple', 'iPhone 14', '128GB', 6.1, '8GB', 'Space Grey', 1099.99, 'Verizon', 'A10001B', 2000),
+#     ('Apple', 'iPhone 14', '128GB', 6.1, '8GB', 'Deep Purple', 1099.99, 'Verizon', 'A10001B', 2001),
+#     ('Apple', 'iPhone 14', '128GB', 6.1, '8GB', 'Alpine Green', 1099.99, 'Verizon', 'A10001B', 2002),
+#     ('Samsung', 'S22', '128GB', 6.6, '8GB', 'Phantom White', 999.99, 'AT&T', 'S10001A', 1000),
+#     ('Samsung', 'S22', '128GB', 6.6, '8GB', 'Bora Purple', 999.99, 'AT&T', 'S10001A', 1001),
+#     ('Samsung', 'S22', '128GB', 6.6, '8GB', 'Pink Gold', 999.99, 'AT&T', 'S10001A', 1002),
+#     ('Samsung', 'S22', '128GB', 6.6, '8GB', 'Phantom White', 999.99, 'Verizon', 'S10001A', 2000),
+#     ('Samsung', 'S22', '128GB', 6.6, '8GB', 'Bora Purple', 999.99, 'Verizon', 'S10001A', 2001),
+#     ('Samsung', 'S22', '128GB', 6.6, '8GB', 'Pink Gold', 999.99, 'Verizon', 'S10001A', 2002)]
+
+# Uncomment out to insert the data above
+# cur.executemany("INSERT INTO SmartPhone VALUES(?,?,?,?,?,?,?,?,?,?);", rows)
+# connection.commit()
+#print("Data inserted Successfully")
+###############################################
 
 
 # Query search here
-res = cur.execute("SELECT Model FROM SmartPhones")
+res = cur.execute('SELECT Brand, Model, Color, Price, Carrier FROM SmartPhone WHERE Price < 1000 AND Carrier = "Verizon"')
 showResult = res.fetchall()
-print(showResult)
+#print(showResult)
+
+print('------------Your Query Below-------------','\n'
+      '-------------SQL To Python---------------', '\n')
+
+count = 0
+for i in showResult:
+    count +=1
+    print(i)
+print("\nTotal Number of Results", count)
+print('\n')
+connection.close()
 # saves the changes to the database
 #connection.commit()
